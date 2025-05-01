b6a9e5205e63384abd5c68e7c8c787a76c89a361
mubarak23 <mubarakaminu340@gmail.com>
2025-05-01 12:43:21 +0100

    switch from sqlite3 to pg

--- knexfile.js
+++ knexfile.js
@@ -3,22 +3,9 @@ dotenv.config();
 
 const config = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: process.env.DB_FILENAME,
-    },
-    useNullAsDefault: true,
-  },
-  production: {
     client: "pg",
-    connection: {
-      connectionString: process.env.DB_URL,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
-    },
+    connection: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false }, // Optional, ensures SSL with Neon
     pool: {
       min: 2,
       max: 10,
@@ -27,7 +14,18 @@ const config = {
       tableName: "knex_migrations",
     },
   },
-  useNullAsDefault: true,
+  production: {
+    client: "pg",
+    connection: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
 
 export default config;
